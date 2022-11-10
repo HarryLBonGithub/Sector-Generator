507e2db498815e156718cac7fe123248e9fffab0
Harry L-B <harryloefflerbell@gmail.com>
2022-11-10 10:01:41 -0600

    created resource pools

---
+++ SecGenSources.py
@@ -0,0 +1,21 @@
+starNames = ["Apion","Betlin","Coriv","Drak-lun","Ert","Fouk","Grint",
+"Hadrol","Inz","Jul","Kanst","Lence","Mawst","Nyx","Olm","Phy","Qul",
+"Rav","Sepht","Tich","Uld","Vog","Wark","Xyxl","Yiur","Zith"]
+
+starSizes = ["small", "mid", "large"]
+
+planetSuffixes = ["Alpha","Beta","Gamma","Delta","Epsilon","Zeta",
+"Eta","Theta","Iota","Kappa","Lambda","Mu","Nu","Xi","Omicron","Pi",
+"Rho","Sigma","Tau","Upsilon","Phi","Chi","Psi","Omega"]
+
+planetSizes = ["small","mid","large"]
+
+planetTemp = ["frozen","cold","temperate","hot","boiling"]
+
+planetHumidity = ["arid","dry","average","wet","flooded"]
+
+planetLifeSigns = ["lifeless","single-cellular","multicellular",
+"intelligent: primitive","intelligent: industrial","intelligent: advanced"]
+
+planetNote = ["acid rain","alien ruins","time fluctuations","eternal night","eternal day",
+"cursed","floating rocks","tunnel network","out of phase","spaceship graveyard","rare mineral"]
